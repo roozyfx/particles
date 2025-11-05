0ee465b82963a078492ada9407ca4342e1849f96
roozyfx <roozyfx@gmail.com>
2025-11-05 11:40:26 +0100

    Add googlebenchmark support

---
+++ benchmark/benchmark_0.cpp
@@ -0,0 +1,11 @@
+#include <benchmark/benchmark.h>
+
+// Define benchmark
+static void BM_ParticleUpdate(benchmark::State& state) {
+  // TODO
+}
+
+// Register the function as a benchmark
+BENCHMARK(BM_ParticleUpdate);
+
+BENCHMARK_MAIN();
