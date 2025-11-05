d05fa471b02220af41fcd2f6d82edbcc2cd507b1
roozyfx <roozyfx@gmail.com>
2025-11-05 12:52:12 +0100

    Particle System, SoA, execution policy par support

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,102 @@
-int main() { return 0; }
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <print>
+#include <vector>
+
+#include "sim/particle.h"
+#include "sim/particles.h"
+#include "sim/particles_aos.h"
+#include "sim/vectors.h"
+#include "utils/logger.h"
+#include "utils/rng.h"
+
+// To try the Array of Structs implementation
+void ArrayOfStruct() {
+  const size_t num_particles{100};
+  RNG<Point3d> rng_points;
+  std::vector<Point3d> points = rng_points.GenerateUniformRandom(
+      num_particles, Point3d(-1, -1, -1), Point3d(1, 1, 1));
+  RNG<Vec3d> rng_vecs;
+  std::vector<Vec3d> velocities = rng_vecs.GenerateUniformRandom(
+      num_particles, Vec3d(-10, -10, -10), Vec3d(10, 10, 10));
+  RNG<double> rng_mass;
+  std::vector<double> masses =
+      rng_mass.GenerateUniformRandom(num_particles, 1, 200);
+
+  std::vector<Particle> particles;
+  particles.reserve(num_particles);
+
+  for (size_t i = 0; i < num_particles; ++i) {
+    particles.emplace_back(Particle(points[i], masses[i], velocities[i], i));
+  }
+  std::vector<Vec3d> F_ext = rng_vecs.GenerateUniformRandom(
+      num_particles, Vec3d(-1, -5, -5), Vec3d(1, 5, 5));
+
+  std::unique_ptr<ParticleSystemAoS> ps =
+      std::make_unique<ParticleSystemAoS>(std::move(particles));
+
+  const auto start{std::chrono::system_clock::now()};
+
+  Vec3d F_global(0, -200, 0);
+  DType d_t = 1;
+  const size_t cycles{2};
+  for (size_t steps = 0; steps < cycles; ++steps) {
+    ps->UpdateN2(F_ext, F_global, d_t);
+    // if (static_cast<long long>(t) % 5 == 0)
+    // std::cout << *ps << std::endl;
+  }
+  const auto duration = std::chrono::system_clock::now() - start;
+  std::cout << "Took "
+            << std::chrono::round<std::chrono::milliseconds>(duration)
+            << " for " << cycles << std::endl;
+}
+
+// TODO earth-sun particle system
+/** Earth - Sun system,
+ * with Sun at(0, 0, 0) and not moving.* earth doesn't move as expected
+ */
+void SunEarthSystem() {
+  DType earth_to_sun{1.4904e11};
+  Point3d pos_earth(earth_to_sun, 0, 0);
+  DType earth_mass{5.972e24};
+  Vec3d earth_v0(0, 29.78e3, 0);
+  Particle earth(pos_earth, earth_mass, earth_v0, 0);
+
+  Point3d pos_sun(0, 0, 0);
+  DType sun_mass{1.989e30};
+  Vec3d sun_v0(0, 0, 0);
+  Particle sun(pos_sun, sun_mass, sun_v0, 1);
+
+  std::vector<Particle> sun_earth{sun};
+}
+
+int main() {
+  const size_t num_particles{4000};
+  const double d_t{1.};
+  Particles<> par_sys(num_particles, d_t);
+
+  std::vector<double> Fext_x(num_particles);
+  std::vector<double> Fext_y(num_particles);
+  std::vector<double> Fext_z(num_particles);
+#ifdef DEBUG
+  std::fill_n(std::begin(Fext_x), num_particles, 0.);
+  std::fill_n(std::begin(Fext_y), num_particles, 0.);
+  std::fill_n(std::begin(Fext_z), num_particles, 0.);
+#else
+  RNG<double> rng;
+  rng.GenerateUniformRandom(Fext_x.data(), num_particles, 0., 0.);
+  rng.GenerateUniformRandom(Fext_y.data(), num_particles, 0., 0.);
+  rng.GenerateUniformRandom(Fext_z.data(), num_particles, 0., 0.);
+#endif
+  auto start_the_clock = std::chrono::system_clock::now();
+  for (size_t step = 0; step < 200; ++step) {
+    par_sys.Update(Fext_x, Fext_y, Fext_z, 0., 0., 0.);
+  }
+  auto duration = std::chrono::system_clock::now() - start_the_clock;
+  auto xx = std::chrono::round<std::chrono::milliseconds>(duration);
+  std::println("Took: {}", xx);
+
+  return 0;
+}
---
+++ src/sim/particles.h
@@ -0,0 +1,208 @@
+#pragma once
+
+#include <cmath>
+#include <concepts>
+#include <iostream>
+// #include <print>
+#include <cstdio>
+#include <vector>
+#ifdef PARALLEL
+#include <execution>
+#define PAR std::execution::par,
+#else
+#define PAR
+#endif
+
+#include "../utils/rng.h"
+#include "constants.hpp"
+
+/**
+ * A Particle System, as a Struct of Arrays of properties
+ */
+template <std::floating_point T = double>
+struct Particles {
+  // number of particles
+  size_t n;
+  T d_t;
+  using VecT = std::vector<T>;
+  // Positions
+  VecT x, y, z;
+  // Masses
+  VecT m;
+  VecT Gm;
+  // velocity
+  VecT vx, vy, vz;
+
+  // acceleration, Beschleunigung
+  VecT ax, ay, az;
+  // Force on each particle. This is temporary, if I choose Gravity between
+  // particles as a the simulated force.
+  VecT Fx, Fy, Fz;
+  static T constexpr epsilon{T(0.0001)};
+  static T constexpr eps2{epsilon * epsilon};
+
+  Particles(const size_t n, const T d_t)
+      : n{n},
+        d_t{d_t},
+        x{VecT(n)},
+        y{VecT(n)},
+        z{VecT(n)},
+        m{VecT(n)},
+        Gm{VecT(n)},
+        vx{VecT(n)},
+        vy{VecT(n)},
+        vz{VecT(n)},
+        ax{VecT(n)},
+        ay{VecT(n)},
+        az{VecT(n)},
+        Fx{VecT(n)},
+        Fy{VecT(n)},
+        Fz{VecT(n)} {
+    Randomize();
+    std::transform(std::begin(m), std::end(m), std::begin(Gm),
+                   [](auto mi) { return G * mi; });
+  }
+
+  Particles(const Particles&) = delete;
+  Particles& operator=(const Particles&) = delete;
+  Particles(Particles&&) noexcept = default;
+  Particles& operator=(Particles&&) noexcept = default;
+  ~Particles() = default;
+  /**
+   * @brief Updates this particle's velocity and position, using a semi-Euler
+   * method
+   *
+   */
+  // @todo: Make different options for Force, F. such as gravity between
+  // particles, no inner-force just initial velocity and global (earth) gravity,
+  // etc.
+  void Update(const VecT& F_ext_x, const VecT& F_ext_y, const VecT& F_ext_z,
+              const T& F_global_x, const T& F_global_y, const T& F_global_z) {
+    UpdateForces(F_ext_x, F_ext_y, F_ext_z, F_global_x, F_global_y, F_global_z);
+
+    UpdateState();
+  }
+
+ private:
+  void UpdateForces(const VecT& F_ext_x, const VecT& F_ext_y,
+                    const VecT& F_ext_z, const T& F_global_x,
+                    const T& F_global_y, const T& F_global_z) {
+    std::fill(begin(Fx), end(Fx), T(0));
+    std::fill(begin(Fy), end(Fy), T(0));
+    std::fill(begin(Fz), end(Fz), T(0));
+
+    for (size_t i = 0; i < n; ++i) {
+      for (size_t j = i + 1; j < n; ++j) {
+        const T rx{x[j] - x[i]};
+        const T ry{y[j] - y[i]};
+        const T rz{z[j] - z[i]};
+
+        const T r2{rx * rx + ry * ry + rz * rz + eps2};
+        using std::sqrt;
+        const T inv_r{T(1.) / sqrt(r2)};
+        T inv_r3 = inv_r * inv_r * inv_r;
+
+        T F = Gm[i] * m[j] * inv_r3;
+        T fx{F * rx};
+        T fy{F * ry};
+        T fz{F * rz};
+
+        // Update forces
+        Fx[i] += fx;
+        Fx[j] -= fx;
+        Fy[i] += fy;
+        Fy[j] -= fy;
+        Fz[i] += fz;
+        Fz[j] -= fz;
+      }
+    }
+
+    // TODO use STL
+    // Add external Force to each particle, if exists
+    if (!F_ext_x.empty())
+      for (size_t i = 0; i < n; ++i) Fx[i] += F_ext_x[i];
+    if (!F_ext_y.empty())
+      for (size_t i = 0; i < n; ++i) Fy[i] += F_ext_y[i];
+    if (!F_ext_z.empty())
+      for (size_t i = 0; i < n; ++i) Fz[i] += F_ext_z[i];
+    // Add global force
+    for (size_t i = 0; i < n; ++i) {
+      Fx[i] += F_global_x;
+      Fy[i] += F_global_y;
+      Fz[i] += F_global_z;
+    }
+  }
+
+  /**
+   * @brief Semi-Euler update on this particle's velocity and position.
+   */
+  void UpdateState() {
+    using std::begin;
+    using std::end;
+
+    // Update acceleration of each particle
+    VecT inv_m;
+    inv_m.reserve(n);
+    std::for_each_n(PAR begin(m), n, [](T& el) { return T(1.) / el; });
+    // acceleration = Force / m
+    std::transform(PAR begin(Fx), end(Fx), begin(inv_m), begin(ax),
+                   std::multiplies<T>());
+    std::transform(PAR begin(Fy), end(Fy), begin(inv_m), begin(ay),
+                   std::multiplies<T>());
+    std::transform(PAR begin(Fz), end(Fz), begin(inv_m), begin(az),
+                   std::multiplies<T>());
+
+    // velocity += acceleration * d_t
+    std::transform(PAR begin(ax), end(ax), begin(vx), begin(vx),
+                   [dt = d_t](const T& a, T& v) { return std::fma(a, dt, v); });
+    std::transform(PAR begin(ay), end(ay), begin(vy), begin(vy),
+                   [dt = d_t](const T& a, T& v) { return std::fma(a, dt, v); });
+    std::transform(PAR begin(az), end(az), begin(vz), begin(vz),
+                   [dt = d_t](const T& a, T& v) { return std::fma(a, dt, v); });
+
+    // position += velocity * d_t
+    std::transform(PAR begin(vx), end(vx), begin(x), begin(x),
+                   [dt = d_t](const T& v, T& p) { return std::fma(v, dt, p); });
+    std::transform(PAR begin(vy), end(vy), begin(y), begin(y),
+                   [dt = d_t](const T& v, T& p) { return std::fma(v, dt, p); });
+    std::transform(PAR begin(vz), end(vz), begin(z), begin(z),
+                   [dt = d_t](const T& v, T& p) { return std::fma(v, dt, p); });
+  }
+
+  // Temporary for testing purposes
+  // TODO: parametrize. Maybe not a very bad function to have, after all.
+  void Randomize() {
+    RNG<T> rng;
+    rng.GenerateUniformRandom(x.data(), n, -2., 2.);
+    rng.GenerateUniformRandom(y.data(), n, -2., 2.);
+    rng.GenerateUniformRandom(z.data(), n, -2., 2.);
+
+    rng.GenerateUniformRandom(m.data(), n, 1., 200.);
+
+    rng.GenerateUniformRandom(vx.data(), n, -20., 20.);
+    rng.GenerateUniformRandom(vy.data(), n, -20., 20.);
+    rng.GenerateUniformRandom(vz.data(), n, -20., 20.);
+  }
+
+ public:
+  // TODO: Possibly useless after debug. Clean-up
+  void Print(const size_t begin, const size_t end) const {
+    for (size_t i = begin; i <= end; ++i) {
+      // unfortunately current implementation of std::println is slow
+      // TODO: change back to println when issues fixed.
+      // std::println(
+      //     "particle: {}--> x: {}, y: {}, z: {}, m: {}, vx: {}, vy: {}, vz:
+      //     {}", i, x[i], y[i], z[i], m[i], vx[i], vy[i], vz[i]);
+      // std::println("    Fx: {}, Fy: {}, Fz: {}, ax: {}, ay: {}, az: {}",
+      // Fx[i],
+      //              Fy[i], Fz[i], ax[i], ay[i], az[i]);
+      // std::println("============================");
+      // std::printf("i: %x x: %.2f", i, x[i]);
+      std::printf(
+          "particle %zd\tp(%.2f,%.2f, %.2f)\tm: %.2f\tv(%.2f, %.2f,%.2f)\n", i,
+          x[i], y[i], z[i], m[i], vx[i], vy[i], vz[i]);
+      std::printf("\t\ta(%.2f,%.2f, %.2f)\tF(%.2f, %.2f,%.2f)\n", ax[i], ay[i],
+                  az[i], Fx[i], Fy[i], Fz[i]);
+    }
+  }
+};
